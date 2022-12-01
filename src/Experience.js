d5baed2b2ad1486692645c26c3e53fc73763d27b
Oleksii Bardyshev <oleksii.bardyshev@unilimes.com>
2022-12-01 12:35:08 +0200

    Environment was added to the scene

--- src/Experience.js
+++ src/Experience.js
@@ -1,20 +1,15 @@
-import {OrbitControls, useGLTF} from '@react-three/drei';
+import {Environment, OrbitControls, useGLTF} from '@react-three/drei';
 
 export default function Experience() {
   const computer = new useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
   console.log(computer)
 
   return <>
-    <color args={ [ '#695b5b' ] } attach="background" />
+    <Environment preset="city" />
+      <color args={ [ '#695b5b' ] } attach="background" />
 
-    <OrbitControls makeDefault />
+      <OrbitControls makeDefault />
 
-    <mesh>
-      <boxGeometry />
-      <meshNormalMaterial />
-    </mesh>
-
-    <primitive object={ computer.scene } />
-
+      <primitive object={ computer.scene } />
     </>
 }

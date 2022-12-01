e77104a18acfa5e74861931311d4f36ac7e39ace
Oleksii Bardyshev <oleksii.bardyshev@unilimes.com>
2022-12-01 12:42:05 +0200

    Floating animation was added to the laptop

--- src/Experience.js
+++ src/Experience.js
@@ -1,4 +1,4 @@
-import {Environment, OrbitControls, useGLTF} from '@react-three/drei';
+import {Environment, Float, OrbitControls, useGLTF} from '@react-three/drei';
 
 export default function Experience() {
   const computer = new useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
@@ -6,10 +6,15 @@ export default function Experience() {
 
   return <>
     <Environment preset="city" />
-      <color args={ [ '#695b5b' ] } attach="background" />
+    <color args={ [ '#695b5b' ] } attach="background" />
 
-      <OrbitControls makeDefault />
+    <OrbitControls makeDefault />
 
-      <primitive object={ computer.scene } />
+    <Float rotationIntensity={ 0.4 }>
+      <primitive
+        object={ computer.scene }
+        position-y={ -1.2 }
+      />
+    </Float>
     </>
 }

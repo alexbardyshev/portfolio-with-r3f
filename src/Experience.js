3fa7e9d27b32939ea11acdf2416def60120c9be4
Oleksii Bardyshev <oleksii.bardyshev@unilimes.com>
2022-12-01 13:04:33 +0200

    Camera and controls were added

--- src/Experience.js
+++ src/Experience.js
@@ -9,6 +9,11 @@ export default function Experience() {
     <color args={ [ '#695b5b' ] } attach="background" />
     <PresentationControls
       global
+      rotation={ [ 0.13, 0.1, 0 ] }
+      polar={ [ -0.4, 0.2] }
+      azimuth={ [ -1, 0.75 ] }
+      config={ { mass: 2, tension: 400 } }
+      snap={ { mass: 4, tension: 400 } }
     >
       <Float rotationIntensity={ 0.4 }>
         <primitive
